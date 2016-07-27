17bd553115f781fa855d46b80334595c0fc60fd2
KSJ <smart23033@gmail.com>
2016-07-27 13:59:27 +0900

    Drawer

---
+++ app/src/main/java/example/tacademy/com/sampledesignsupport/DrawerActivity.java
@@ -0,0 +1,90 @@
+package example.tacademy.com.sampledesignsupport;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    DrawerLayout drawer;
+    TextView messageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+        drawer = (DrawerLayout)findViewById(R.id.drawer);
+        messageView = (TextView)findViewById(R.id.text_message);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(android.R.drawable.ic_dialog_map);
+        TextView menu = (TextView)findViewById(R.id.menu_1);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContent("Menu 1");
+            }
+        });
+
+        menu = (TextView)findViewById(R.id.menu_2);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContent("Menu 2");
+            }
+        });
+
+        menu = (TextView)findViewById(R.id.menu_3);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showContent("Menu 3");
+            }
+        });
+
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Toast.makeText(DrawerActivity.this,"drawer opened",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Toast.makeText(DrawerActivity.this,"drawer closed",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+
+    private void showContent(String message){
+        messageView.setText(message);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            if(drawer.isDrawerOpen(GravityCompat.START)){
+                drawer.closeDrawer(GravityCompat.START);
+            }else{
+                drawer.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
